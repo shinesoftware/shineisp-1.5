78379f8bd456203e17caee4e16017f6fb57179fa
Michelangelo Turillo <mturillo@shinesoftware.com>
2014-05-24 20:02:40 +0200

    Cms Block has been created

--- module/Cms/data/data.sql
+++ module/Cms/data/data.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: May 23, 2014 at 10:11 PM
+-- Generation Time: May 24, 2014 at 07:01 PM
 -- Server version: 5.6.17
 -- PHP Version: 5.4.24
 
@@ -11,15 +11,29 @@ SET FOREIGN_KEY_CHECKS=0;
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
 --
 -- Database: `shineisp2`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `block`
+--
+
+DROP TABLE IF EXISTS `block`;
+CREATE TABLE IF NOT EXISTS `block` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) NOT NULL,
+  `placeholder` varchar(100) NOT NULL,
+  `content` text,
+  `visible` tinyint(1) NOT NULL DEFAULT '1',
+  `language_id` int(11) DEFAULT NULL,
+  `createdat` datetime DEFAULT NULL,
+  `updatedat` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `language_id` (`language_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -33,6 +47,7 @@ CREATE TABLE IF NOT EXISTS `page` (
   `title` varchar(255) NOT NULL,
   `slug` varchar(255) NOT NULL,
   `category_id` int(11) DEFAULT NULL,
+  `language_id` int(11) DEFAULT NULL,
   `visible` tinyint(1) NOT NULL DEFAULT '1',
   `tags` varchar(255) DEFAULT NULL,
   `createdat` datetime DEFAULT NULL,
@@ -41,7 +56,8 @@ CREATE TABLE IF NOT EXISTS `page` (
   `parent_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `category_id` (`category_id`),
-  KEY `parent_id` (`parent_id`)
+  KEY `parent_id` (`parent_id`),
+  KEY `language_id` (`language_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -63,13 +79,16 @@ CREATE TABLE IF NOT EXISTS `page_category` (
 --
 
 --
+-- Constraints for table `block`
+--
+ALTER TABLE `block`
+  ADD CONSTRAINT `block_ibfk_1` FOREIGN KEY (`language_id`) REFERENCES `languages` (`id`) ON DELETE SET NULL;
+
+--
 -- Constraints for table `page`
 --
 ALTER TABLE `page`
+  ADD CONSTRAINT `page_ibfk_3` FOREIGN KEY (`language_id`) REFERENCES `languages` (`id`) ON DELETE SET NULL,
   ADD CONSTRAINT `page_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `page_category` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `page_ibfk_2` FOREIGN KEY (`parent_id`) REFERENCES `page` (`id`) ON DELETE SET NULL ON UPDATE SET NULL;
 SET FOREIGN_KEY_CHECKS=1;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
