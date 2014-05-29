5123a8320a9c9e3135c05f9e628b2e3163dd594d
ShineISP <info@shineisp.com>
2014-05-29 08:27:43 +0200

    Base SQL dump updated

--- module/Base/data/data.sql
+++ module/Base/data/data.sql
@@ -31,4 +31,120 @@ INSERT INTO `languages` (`id`, `language`, `locale`, `code`, `base`, `active`) V
 (6, 'German', 'de_DE', 'de', 0, 1),
 (7, 'Greek', 'el_GR', 'gr', 0, 0),
 (8, 'Hungarian', 'hu_HU', 'hu', 0, 0);
+
+
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `display_name` varchar(50) DEFAULT NULL,
+  `password` varchar(128) NOT NULL,
+  `state` smallint(5) unsigned DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_password_reset`
+--
+
+DROP TABLE IF EXISTS `user_password_reset`;
+CREATE TABLE IF NOT EXISTS `user_password_reset` (
+  `request_key` varchar(32) NOT NULL,
+  `user_id` int(11) unsigned NOT NULL,
+  `request_time` datetime NOT NULL,
+  PRIMARY KEY (`request_key`),
+  UNIQUE KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_provider`
+--
+
+DROP TABLE IF EXISTS `user_provider`;
+CREATE TABLE IF NOT EXISTS `user_provider` (
+  `user_id` int(10) unsigned NOT NULL,
+  `provider_id` varchar(50) NOT NULL,
+  `provider` varchar(255) NOT NULL,
+  PRIMARY KEY (`user_id`,`provider_id`),
+  UNIQUE KEY `provider_id` (`provider_id`,`provider`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_role`
+--
+
+DROP TABLE IF EXISTS `user_role`;
+CREATE TABLE IF NOT EXISTS `user_role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `role_id` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `is_default` tinyint(1) NOT NULL DEFAULT '0',
+  `parent_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_role` (`role_id`),
+  KEY `idx_parent_id` (`parent_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `user_role`
+--
+
+INSERT INTO `user_role` (`id`, `role_id`, `is_default`, `parent_id`) VALUES
+(1, 'guest', 1, NULL),
+(2, 'user', 0, 1),
+(3, 'admin', 0, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_role_linker`
+--
+
+DROP TABLE IF EXISTS `user_role_linker`;
+CREATE TABLE IF NOT EXISTS `user_role_linker` (
+  `user_id` int(10) unsigned NOT NULL,
+  `role_id` int(11) NOT NULL,
+  PRIMARY KEY (`user_id`,`role_id`),
+  KEY `idx_role_id` (`role_id`),
+  KEY `idx_user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `user_password_reset`
+--
+ALTER TABLE `user_password_reset`
+  ADD CONSTRAINT `user_password_reset_fk1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `user_provider`
+--
+ALTER TABLE `user_provider`
+  ADD CONSTRAINT `user_provider_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `user_role`
+--
+ALTER TABLE `user_role`
+  ADD CONSTRAINT `fk_parent_id` FOREIGN KEY (`parent_id`) REFERENCES `user_role` (`id`) ON DELETE SET NULL;
+
+--
+-- Constraints for table `user_role_linker`
+--
+ALTER TABLE `user_role_linker`
+  ADD CONSTRAINT `fk_role_id` FOREIGN KEY (`role_id`) REFERENCES `user_role` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE;
+  
 SET FOREIGN_KEY_CHECKS=1;
