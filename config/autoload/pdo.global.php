f3ab32556522034b445d4f15f6852b2d90674b2c
Michelangelo Turillo <mturillo@shinesoftware.com>
2014-08-11 11:01:20 +0200

    global file fixed #4

--- config/autoload/pdo.global.php
+++ config/autoload/pdo.global.php
@@ -12,44 +12,4 @@
  */
 
 
-$dbParams = array(
-        'hostname'  => 'localhost',
-        'driver' => 'Pdo',
-        'database' => 'shineisp2',
-        'username' => 'root',
-        'password' => '11041977',
-        // buffer_results - only for mysqli buffered queries, skip for others
-        'options' => array('buffer_results' => true),
-);
-return array(
-        'service_manager' => array(
-                'factories' => array(
-                        'Zend\Db\Adapter\Adapter' => function ($sm) use ($dbParams) {
-                            $adapter = new BjyProfiler\Db\Adapter\ProfilingAdapter(array(
-                                    'driver'    => 'pdo',
-                                    'dsn'       => 'mysql:dbname='.$dbParams['database'].';host='.$dbParams['hostname'],
-                                    'username'  => $dbParams['username'],
-                                    'password'  => $dbParams['password'],
-                                    PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8'
-                            ));
-
-                            if (php_sapi_name() == 'cli') {
-                                $logger = new Zend\Log\Logger();
-                                // write queries profiling info to stdout in CLI mode
-                                $writer = new Zend\Log\Writer\Stream('php://output');
-                                $logger->addWriter($writer, Zend\Log\Logger::DEBUG);
-                                $adapter->setProfiler(new BjyProfiler\Db\Profiler\LoggingProfiler($logger));
-                            } else {
-                                $adapter->setProfiler(new BjyProfiler\Db\Profiler\Profiler());
-                            }
-                            if (isset($dbParams['options']) && is_array($dbParams['options'])) {
-                                $options = $dbParams['options'];
-                            } else {
-                                $options = array();
-                            }
-                            $adapter->injectProfilingStatementPrototype($options);
-                            return $adapter;
-                        },
-                ),
-        ),
-);
+return array();
