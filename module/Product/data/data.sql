268b6416a28f979a3f9d0164bd63f8f5daf286e5
Michelangelo Turillo <mturillo@shinesoftware.com>
2014-09-02 14:52:33 +0200

    Product attribute set management has been updated

--- module/Product/data/data.sql
+++ module/Product/data/data.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 13, 2014 at 04:34 PM
+-- Generation Time: Sep 02, 2014 at 01:52 PM
 -- Server version: 5.6.17
 -- PHP Version: 5.4.24
 
@@ -32,14 +32,20 @@ CREATE TABLE IF NOT EXISTS `product` (
   PRIMARY KEY (`id`),
   KEY `group_id` (`attribute_set_id`),
   KEY `type_id` (`type_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
 
 --
 -- Dumping data for table `product`
 --
 
 INSERT INTO `product` (`id`, `uid`, `type_id`, `attribute_set_id`, `createdat`, `updatedat`) VALUES
-(2, '22ff2755-1540-4d68-8195-079356c33e22', 1, 2, '2014-07-08 10:21:40', '2014-08-13 16:32:26');
+(2, '22ff2755-1540-4d68-8195-079356c33e22', 1, 2, '2014-07-08 10:21:40', '2014-09-02 13:29:14'),
+(3, '2ad38cd7-be51-43f5-8f98-9564e9a4b341', 1, 1, '2014-08-27 16:26:10', '2014-08-27 16:26:10'),
+(4, '6315bd2c-83fc-473e-ba6b-47e4783dfaaa', 1, 1, '2014-08-27 16:29:10', '2014-08-27 16:29:10'),
+(5, '63e023b7-6253-4c5a-b38a-4b629e1b704e', 1, 1, '2014-08-27 16:29:41', '2014-08-27 16:29:41'),
+(6, 'b4490416-d41e-43a5-998d-1c88e97a1012', 1, 1, '2014-08-27 16:30:02', '2014-08-27 16:30:02'),
+(7, '7a45aee9-9acb-4057-b88e-e652d695edf9', 1, 1, '2014-08-27 16:30:21', '2014-08-27 16:30:21'),
+(8, '3f3a0014-c90b-4bdb-b936-ec863a00f9cd', 1, 1, '2014-08-27 16:32:19', '2014-08-28 08:38:27');
 
 -- --------------------------------------------------------
 
@@ -57,30 +63,35 @@ CREATE TABLE IF NOT EXISTS `product_attributes` (
   `label` varchar(200) NOT NULL,
   `source_model` varchar(200) DEFAULT NULL,
   `filters` varchar(255) DEFAULT NULL,
+  `validators` text,
+  `filetarget` varchar(255) DEFAULT NULL,
+  `filesize` int(11) DEFAULT NULL,
+  `filemimetype` text,
   `is_required` tinyint(1) NOT NULL DEFAULT '0',
   `is_user_defined` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=16 ;
 
 --
 -- Dumping data for table `product_attributes`
 --
 
-INSERT INTO `product_attributes` (`id`, `name`, `type`, `input`, `css`, `label`, `source_model`, `filters`, `is_required`, `is_user_defined`) VALUES
-(1, 'name', 'string', 'text', NULL, 'Name', '', NULL, 1, 0),
-(2, 'sku', 'string', 'text', NULL, 'SKU', NULL, '["stringtolower"]', 1, 0),
-(3, 'metadescription', 'text', 'textarea', NULL, 'META Description', NULL, NULL, 0, 0),
-(4, 'metakeyword', 'string', 'text', NULL, 'META Keywords', NULL, NULL, 0, 0),
-(5, 'metatitle', 'string', 'text', NULL, 'META Title', NULL, NULL, 0, 0),
-(6, 'status', 'integer', 'select', NULL, 'Status', '\\Base\\Form\\Element\\Enadisabled', NULL, 1, 0),
-(7, 'urlkey', 'string', 'text', NULL, 'URL Key', NULL, '["stringtolower"]', 0, 0),
-(8, 'news_from_date', 'date', 'text', 'date', 'Set Product as New from Date', NULL, NULL, 0, 0),
-(9, 'news_to_date', 'date', 'text', 'date', 'Set Product as New to Date', NULL, NULL, 0, 0),
-(10, 'short_description', 'text', 'textarea', NULL, 'Short Description', NULL, NULL, 1, 0),
-(11, 'description', 'text', 'textarea', NULL, 'Description', NULL, NULL, 1, 0),
-(12, 'price', 'float', 'text', NULL, 'Price', NULL, NULL, 1, 0),
-(13, 'special_price', 'string', 'text', NULL, 'Special Price', NULL, NULL, 0, 0),
-(14, 'webspace', 'string', 'text', '', 'Web Space', NULL, '["int"]', 0, 1);
+INSERT INTO `product_attributes` (`id`, `name`, `type`, `input`, `css`, `label`, `source_model`, `filters`, `validators`, `filetarget`, `filesize`, `filemimetype`, `is_required`, `is_user_defined`) VALUES
+(1, 'name', 'string', 'text', NULL, 'Name', '', NULL, NULL, '', NULL, NULL, 1, 0),
+(2, 'sku', 'string', 'text', NULL, 'SKU', NULL, '["stringtolower"]', NULL, '', NULL, NULL, 1, 0),
+(3, 'metadescription', 'text', 'textarea', NULL, 'META Description', NULL, NULL, NULL, '', NULL, NULL, 0, 0),
+(4, 'metakeyword', 'string', 'text', NULL, 'META Keywords', NULL, NULL, NULL, '', NULL, NULL, 0, 0),
+(5, 'metatitle', 'string', 'text', NULL, 'META Title', NULL, NULL, NULL, '', NULL, NULL, 0, 0),
+(6, 'status', 'integer', 'select', NULL, 'Status', '\\Base\\Form\\Element\\Enadisabled', NULL, NULL, '', NULL, NULL, 1, 0),
+(7, 'urlkey', 'string', 'text', NULL, 'URL Key', NULL, '["stringtolower"]', NULL, '', NULL, NULL, 0, 0),
+(8, 'news_from_date', 'date', 'text', 'form-control date', 'Set Product as New from Date', NULL, NULL, NULL, '', NULL, NULL, 0, 0),
+(9, 'news_to_date', 'date', 'text', 'form-control date', 'Set Product as New to Date', NULL, NULL, NULL, '', NULL, NULL, 0, 0),
+(10, 'short_description', 'text', 'textarea', NULL, 'Short Description', NULL, NULL, NULL, '', NULL, NULL, 1, 0),
+(11, 'description', 'text', 'textarea', NULL, 'Description', NULL, NULL, NULL, '', NULL, NULL, 1, 0),
+(12, 'price', 'float', 'text', NULL, 'Price', NULL, NULL, NULL, '', NULL, NULL, 1, 0),
+(13, 'special_price', 'string', 'text', NULL, 'Special Price', NULL, NULL, NULL, '', NULL, NULL, 0, 0),
+(14, 'webspace', 'string', 'text', '', 'Web Space', NULL, '["int"]', NULL, '', NULL, NULL, 0, 1),
+(15, 'photo', 'string', 'file', 'file', 'Photo', 'Zend\\Form\\Element\\File', '[{"name":"File\\\\RenameUpload","options":{"target":"\\/Library\\/WebServer\\/Documents\\/shineisp2\\/public\\/documents\\/product\\/images\\/","overwrite":true,"use_upload_name":true}}]', '[{"name":"File\\\\UploadFile","filesize":{"max":"100000"},"filemimetype":[{"mimeType":"image\\/jpeg"},{"mimeType":"image\\/gif"},{"mimeType":"image\\/png"}]}]', '/documents/product/images', 100000, '["image\\/jpeg","image\\/gif","image\\/png"]', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -97,15 +108,17 @@ CREATE TABLE IF NOT EXISTS `product_attributes_entity_date` (
   PRIMARY KEY (`id`),
   KEY `entity_id` (`entity_id`),
   KEY `attribute_id` (`attribute_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Dumping data for table `product_attributes_entity_date`
 --
 
 INSERT INTO `product_attributes_entity_date` (`id`, `entity_id`, `attribute_id`, `value`) VALUES
-(1, 2, 8, '2014-08-28'),
-(2, 2, 9, '2014-08-25');
+(1, 2, 8, '2014-09-24'),
+(2, 2, 9, '2014-09-24'),
+(3, 8, 8, '2014-09-03'),
+(4, 8, 9, '2014-08-20');
 
 -- --------------------------------------------------------
 
@@ -139,14 +152,15 @@ CREATE TABLE IF NOT EXISTS `product_attributes_entity_float` (
   PRIMARY KEY (`id`),
   KEY `entity_id` (`entity_id`),
   KEY `attribute_id` (`attribute_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Dumping data for table `product_attributes_entity_float`
 --
 
 INSERT INTO `product_attributes_entity_float` (`id`, `entity_id`, `attribute_id`, `value`) VALUES
-(1, 2, 12, 10.5);
+(1, 2, 12, 10.5),
+(2, 8, 12, 10.5);
 
 -- --------------------------------------------------------
 
@@ -163,14 +177,15 @@ CREATE TABLE IF NOT EXISTS `product_attributes_entity_integer` (
   PRIMARY KEY (`id`),
   KEY `entity_id` (`entity_id`),
   KEY `attribute_id` (`attribute_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Dumping data for table `product_attributes_entity_integer`
 --
 
 INSERT INTO `product_attributes_entity_integer` (`id`, `entity_id`, `attribute_id`, `value`) VALUES
-(1, 2, 6, 1);
+(1, 2, 6, 1),
+(2, 8, 6, 1);
 
 -- --------------------------------------------------------
 
@@ -187,7 +202,7 @@ CREATE TABLE IF NOT EXISTS `product_attributes_entity_string` (
   PRIMARY KEY (`id`),
   KEY `entity_id` (`entity_id`),
   KEY `attribute_id` (`attribute_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=18 ;
 
 --
 -- Dumping data for table `product_attributes_entity_string`
@@ -200,7 +215,16 @@ INSERT INTO `product_attributes_entity_string` (`id`, `entity_id`, `attribute_id
 (4, 2, 13, NULL),
 (5, 2, 7, 'hosting'),
 (6, 2, 5, NULL),
-(7, 2, 14, '100');
+(7, 2, 14, '100'),
+(8, 8, 1, 'Hosting Gold'),
+(9, 8, 2, 'hst-02'),
+(10, 8, 7, 'hosting-gold'),
+(11, 8, 13, '8.60'),
+(12, 8, 4, NULL),
+(13, 8, 5, NULL),
+(15, 2, 8, '2014-09-17'),
+(16, 2, 9, '02/09/17'),
+(17, 2, 15, 'Schermata 2014-09-02 alle 11.34.58.png');
 
 -- --------------------------------------------------------
 
@@ -217,7 +241,7 @@ CREATE TABLE IF NOT EXISTS `product_attributes_entity_text` (
   PRIMARY KEY (`id`),
   KEY `entity_id` (`entity_id`),
   KEY `attribute_id` (`attribute_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
 
 --
 -- Dumping data for table `product_attributes_entity_text`
@@ -226,7 +250,11 @@ CREATE TABLE IF NOT EXISTS `product_attributes_entity_text` (
 INSERT INTO `product_attributes_entity_text` (`id`, `entity_id`, `attribute_id`, `value`) VALUES
 (1, 2, 11, 'test B'),
 (2, 2, 3, NULL),
-(3, 2, 10, 'test A');
+(3, 2, 10, 'test A'),
+(4, 8, 3, NULL),
+(5, 8, 10, 'test'),
+(6, 8, 11, 'test'),
+(7, 2, 15, 'f24-turillo.pdf');
 
 -- --------------------------------------------------------
 
@@ -239,7 +267,7 @@ CREATE TABLE IF NOT EXISTS `product_attributes_groups` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=14 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=16 ;
 
 --
 -- Dumping data for table `product_attributes_groups`
@@ -251,7 +279,9 @@ INSERT INTO `product_attributes_groups` (`id`, `name`) VALUES
 (3, 'Metadata'),
 (11, 'Principale'),
 (12, 'Hosting'),
-(13, 'Content');
+(13, 'Content'),
+(14, 'Custom'),
+(15, 'Images');
 
 -- --------------------------------------------------------
 
@@ -280,19 +310,21 @@ INSERT INTO `product_attributes_idx` (`attribute_group_id`, `attribute_set_id`, 
 (1, 1, 7),
 (1, 1, 8),
 (1, 1, 9),
-(1, 1, 10),
-(1, 1, 11),
 (2, 1, 12),
 (2, 1, 13),
 (3, 1, 4),
 (3, 1, 3),
 (3, 1, 5),
+(15, 1, 15),
+(13, 1, 10),
+(13, 1, 11),
 (1, 2, 1),
 (1, 2, 2),
 (1, 2, 6),
 (1, 2, 7),
 (1, 2, 8),
 (1, 2, 9),
+(15, 2, 15),
 (2, 2, 12),
 (2, 2, 13),
 (3, 2, 3),
